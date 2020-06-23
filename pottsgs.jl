4b6d019c57a31937145555f3263de98ff591b500
Christopher White <christopher.white@alumni.rice.edu>
2020-06-23 07:54:59 -0400

    use potts-sites in gs

--- pottsgs.jl
+++ pottsgs.jl
@@ -6,13 +6,10 @@ using ProgressMeter
 using Serialization
 import ITensors.op
 import ITensors.randomMPS
+include("potts-sites.jl")
 
 git_commit() = String(read(pipeline(`git log`, `head -1`, `cut -d ' ' -f 2`, `cut -b 1-7`))[1:end-1])
 git_commit(path :: String) = cd(git_commit, path)
-
-function pottsSites(N :: Int; q :: Int = 3)
-  return [Index(q, "Site,Potts,n=$n") for n = 1:N]
-end
 
 randomMPS(sites, χ :: Int64) = randomMPS(Float64, sites, χ)
 function randomMPS(::Type{S}, sites, χ :: Int64) where S <: Number
@@ -43,55 +40,6 @@ function ket0_mps(sites)
     return M
 end
 
-const PottsSite = makeTagType("Potts")
-
-# 1-index my Potts states
-# so diagonal elements of Z are
-#    e^{2πi/q}
-#    e^{2πi*2/q}
-#    e^{2πi*3/q}
-#    e^{2πi*(q-1)/q}
-#    e^{2πi*q/q} = 1
-# this seems like the least bad thing
-function state(::PottsSite,
-               st::AbstractString)
-  return parse(Int64, st)
-end
-
-function op(::PottsSite,
-            s :: Index,
-            opname :: AbstractString)::ITensor
-  sP = prime(s)
-  q = dim(s)
-
-  Op = ITensor(Complex{Float64},dag(s), s')
-
-  if opname == "Z"
-    for j in 1:q
-      Op[j,j] = exp(2*π*im*j/q)
-    end
-  elseif opname == "ZH"
-    for j in 1:q
-      Op[j,j] = exp(-2*π*im*j/q)
-    end
-  elseif opname == "X"
-    for j in 1:q
-      Op[(j % q) + 1,j] = 1
-    end
-  elseif opname == "XH"
-    for j in 1:q
-      Op[j,(j % q) + 1] = 1
-    end
-  elseif opname == "X+XH"
-    for j in 1:q
-      Op[j,(j % q) + 1] = 1
-      Op[(j % q) + 1,j] = 1
-    end
-  else
-    throw(ArgumentError("Operator name '$opname' not recognized for PottsSite"))
-  end
-  return Op
-end    
 
 function potts3gs(θ, λ, χ0, sites;
                   ψ0    :: Union{Nothing,MPS} = nothing,
